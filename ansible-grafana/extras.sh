d007a6a12b8edc7ebe7c7b5d5fa7a8955daf7da6
Runamook <tothe8c@gmail.com>
2017-12-10 02:42:21 +0300

    Fixed extras.sh

--- ansible-grafana/extras.sh
+++ ansible-grafana/extras.sh
@@ -3,14 +3,3 @@
 PYTHONPATH=/opt/graphite/webapp django-admin.py migrate --settings=graphite.settings --run-syncdb
 
 sudo cp -r /opt/graphite/webapp/content/* /opt/graphite/static/
-
-#curl -XPOST -H "Accept: application/json" -H "Content-Type: application/json" http://admin:admin@192.168.33.4:3000/api/datasources -d '{
-#  "isDefault" : true,
-#  "name":"local_graphite",
-#  "type":"graphite",
-#  "url":"http://localhost",
-#  "access":"proxy",
-#  "basicAuth":false,
-#  "jsonData" : {
-#          "graphiteVersion" : "1.0"
-#}}'
